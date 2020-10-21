8a61e50c9c48e0394262e48d79006918b4c53d53
Katie <adlerk8@gmail.com>
2020-10-21 08:53:58 -0700

    pushy push

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
+import React, {Component} from 'react';
+import './App';
+import List from './components/List';
 
+class App extends Component {
+    constructor () {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+        <div className='App'>
+            <h1 className='movieList'>The Most Amazing Movie List</h1>
+            <List/>
+        </div>
+        )
+    }
+}
+
+export default App
---
+++ src/components/List.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class List extends Component {
+    constructor() {
+        super();
+
+        this.state = [
+
+        ]
+    }
+
+    render() {
+        return(
+            <div>List Component</div>
+        )
+    }
+}
+
+export default List
